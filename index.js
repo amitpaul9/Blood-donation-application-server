0f33885c32e5bc32adcb8cf087992359c21b9b19
amitpaul9 <atamit905@gmail.com>
2025-12-19 20:46:27 +0600

    fixed firebase token error

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ const port = `${process.env.PORT}` || 3000;
 app.use(cors());
 app.use(express.json());
 
+// firebase sdk
+const admin = require("firebase-admin");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const varifyToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+
+  try {
+    const idToken = token.split(" ")[1];
+    const decoded = await admin.auth().varifyIdToken(idToken);
+    console.log("got id token", decoded);
+    req.decoded_email = decoded.email;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "got error in authorization" });
+  }
+};
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rbs3vpy.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,7 +56,7 @@ async function run() {
 
     //user routes
     //post user data
-    app.post("/users", async (req, res) => {
+    app.post("/users", varifyToken, async (req, res) => {
       const newUser = req.body;
       const email = req.body.email;
       const query = { email: email };
